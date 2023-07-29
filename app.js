4942973b5d58095b361d773d58a499a48813dac2
balajigajendran5 <140400007+balajigajendran5@users.noreply.github.com>
2023-07-29 15:07:36 +0530

    Update app.js
    testing photo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const hostname = '172.31.46.11'; // Your server ip address
 const port = 3000;
 
-const version = '1,000,000';
+const version = '2';
 
 app.get('/', (req, res) => {
     // set response content    
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
                     <body>
                         <h1 style="color:blue;text-align: center;margin-top: 100px;"> [Version ${version}]: NULOgic DevOps Team member BG</h1>
                     
-                        <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%)">
+                        <div style="position: fixed;top: 60%;left: 60%;transform: translate(-50%, -50%)">
                             <img src="https://picsum.photos/400/400?random=1">
                         </div>
                     </body>
